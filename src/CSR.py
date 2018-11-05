a66b2b3db380f50b262debf27aaf661f18340df5
Uriel Hai <urielha@users.noreply.github.com>
2018-11-05 14:45:59 +0200

    fix the CSR

--- src/CSR.py
+++ src/CSR.py
@@ -19,34 +19,36 @@ class CsrFiles:
         self.csr = nx.Graph()
         self.filesMapping = hashing.MagicHash()
 
+    def _supported_type(self, filename):
+        return filename.rsplit(".")[-1] in SUPPORTED_FILE_TYPES
+
     def commit_to_session(self, commit: Models.Commit):
         session = Session(commit.author.email, commit.date_str)
 
         for file in commit.files:
-            if file.filename.rsplit(".")[-1] in SUPPORTED_FILE_TYPES:
-                if file.changetype == Models.ChangeEnum.RENAMED:
-                    if file.source.rsplit(".")[-1] in SUPPORTED_FILE_TYPES:
-                        try:
-                            self.filesMapping.rename(file.source, file.target)
-                        except KeyError:
-                            print(f"! WARNING ! rename file source not found. "
-                                  f"{commit.sha}: {file.source}->{file.target}")
-                            raise
-                    else:
-                        objId = self.filesMapping[file.target]
-                else:
-                    objId = self.filesMapping[file.target]
-                    if file.changetype == Models.ChangeEnum.DELETED:
-                        objId = self.filesMapping[file.source]
-                    elif file.changetype != Models.ChangeEnum.MODIFIED and \
-                            file.changetype != Models.ChangeEnum.ADDED:
-                        raise ValueError(f"Unknown file status: {str(file)}")
-                    session.addAction(Action(objId, file.changetype))
+            if file.changetype == Models.ChangeEnum.RENAMED:
+                try:
+                    self.filesMapping.rename(file.source, file.target)
+                except KeyError:
+                    print(f"! WARNING ! rename file source not found. "
+                          f"{commit.sha}: {file.source}->{file.target}")
+                    raise
+
+            elif file.changetype not in Models.ChangeEnum:
+                raise ValueError(f"Unknown file status: {str(file)}")
+
+            else:
+                objId = self.filesMapping[file.target]
+                if self._supported_type(file.target):
+                    self._inner_process(objId, file, session)
 
         return session
-    #
-    # def addAction(self, objId: int, file: Models.FileChangeset, session: Session):
-    #     session.addAction(Action(objId, file.changetype))
+
+    def _inner_process(self, objId: int, file: Models.FileChangeset, session: Session):
+        """
+        just so derived classes can override behavior
+        """
+        session.addAction(Action(objId, file.changetype))
 
 
 class CsrCode(CsrFiles):
@@ -58,7 +60,7 @@ class CsrCode(CsrFiles):
     def _func_fullname(self, file, func):
         return f"{file}_->_{func}"
 
-    def addAction(self, objId: int, file: Models.FileChangeset, session: Session):
+    def _inner_process(self, objId: int, file: Models.FileChangeset, session: Session):
         functions = set()
         for patch in file.patches:
             func_full_name = self._func_fullname(objId, patch.section_header)
@@ -67,10 +69,11 @@ class CsrCode(CsrFiles):
             func_id = self.functionMapping[func_full_name]
             functions.add(Action(func_id, file.changetype))
 
+            # TODO: fix here - not yield but session.add()
             yield from self.batch_get_functions(objId, Models.ChangeEnum.DELETED,
-                patch.source_lines)
+                                                patch.source_lines)
             yield from self.batch_get_functions(objId, Models.ChangeEnum.ADDED,
-                patch.target_lines)
+                                                patch.target_lines)
 
         yield from functions
 
