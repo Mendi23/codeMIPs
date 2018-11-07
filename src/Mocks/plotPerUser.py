a86bd2eb2697f545f8ad5f64c1050cb3c9f9ef38
Uriel Hai <urielha@users.noreply.github.com>
2018-11-07 21:16:46 +0200

    kuku

--- src/Mocks/plotPerUser.py
+++ src/Mocks/plotPerUser.py
@@ -1,3 +1,5 @@
+import sys
+
 from src.MIP import Mip
 from CSR import CsrFiles
 from Factory import Provider
@@ -5,51 +7,42 @@ from prettytable import PrettyTable as pt
 from DataModule.models import ChangeEnum
 from os import path, mkdir
 from pyutils.file_paths import RESULTS_DIR
+import numpy as np
+import pandas as pd
 
-if __name__ == "__main__":
-    repo = "urielha/heapdict"
-    mip = Mip()
-    csr = CsrFiles()
-    p = Provider(0.8, repo)
 
-    result_folder = path.join(RESULTS_DIR,repo.split('/')[-1])
+def make_result_dir(repo):
+    result_folder = path.join(RESULTS_DIR, repo.split('/')[-1])
     if not path.exists(result_folder):
         mkdir(result_folder)
-
-    table = pt(['commit', 'user', 'changed_objects', 'pred', 'score', 'top 3', 'top 5'])
-
-    X, Y = p.X[0], p.Y[0]
-
-    for i, commit in enumerate(X, 1):
-        mip.updateMIP(csr.commit_to_session(commit))
-
-    for j, commit in enumerate(Y, i):
-
-        session = csr.commit_to_session(commit)
-        objects = session.get_session_objects(ChangeEnum.MODIFIED)
-
-        pred_hits = []
-        score = 0.0
-        total_doi = 0.0
-        mip_ranking = list(mip.rankObjects(session.user))
-
-        for (pred_o, pred_doi) in mip_ranking:
-            total_doi += pred_doi
-            if pred_o in objects:
-                score += pred_doi
-                pred_hits.append(1)
-            else:
-                pred_hits.append(0)
-
-        top_5 = sum(pred_hits[:5]) if len(pred_hits) >= 5 else -1
-        top_3 = sum(pred_hits[:3]) if len(pred_hits) >= 3 else -1
+    return result_folder
 
 
-        table.add_row([j, session.user.split('@', 1)[0],
-                       objects, mip_ranking, score/total_doi, top_3, top_5])
+if __name__ == "__main__":
+    mip = Mip()
+    csr = CsrFiles()
+    p = Provider(1)
 
-        mip.drawMip(path.join(result_folder, str(j)), session.user, objects)
-        mip.updateMIP(session)
+    table = pd.DataFrame(columns=["user", "commits", "accuracy"])
 
-    with open(path.join(result_folder, 'res.txt'), 'w') as f:
-        f.write(str(table))
+    for repo in p.X:
+        make_result_dir(repo.name)
+        for commit in repo:
+            session = csr.commit_to_session(commit)
+            objects = session.get_session_objects(ChangeEnum.MODIFIED)
+
+            pred_hits = []
+            score = 0.0
+            total_doi = 0.0
+            mip_ranking = list(mip.rankObjects(session.user))
+
+            for (pred_o, pred_doi) in mip_ranking:
+                total_doi += pred_doi
+                if pred_o in objects:
+                    score += pred_doi
+                    pred_hits.append(1)
+                else:
+                    pred_hits.append(0)
+
+            mip.updateMIP(session)
+
