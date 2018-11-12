b90572b9ba72d28d7bc2cca6180c78da8a1a85f5
Uriel Hai <urielha@users.noreply.github.com>
2018-11-12 21:20:27 +0200

    visualize is ready?

--- src/AnalysisModule/VisualizeGraph.py
+++ src/AnalysisModule/VisualizeGraph.py
@@ -1,4 +1,5 @@
-from collections import deque, defaultdict
+from collections import deque, defaultdict, namedtuple
+from functools import partial
 
 from src.MIP import Mip
 from CSR import CsrFiles
@@ -7,17 +8,43 @@ from prettytable import PrettyTable as pt
 from DataModule.models import ChangeEnum
 from os import path
 from pyutils.file_paths import get_repo_result_dir
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def draw_users(users_table, users_data, repo_name, result_folder):
+    TOLERANCE = 10
+    table = pd.DataFrame(data=list(users_table),
+                         columns=["user", "commits", "accuracy", "top_3", "top_5"])
+
+    for user in (u for u, v in users_data.items() if v["commits"] > TOLERANCE):
+        u_table = table[table["user"] == user]
+        if u_table.empty: continue
+        plt.figure(clear=True)
+
+        ax = u_table.plot(
+            kind='line', y='accuracy',
+            figsize=(10, 6),
+            title=f"Accuracy per commits | repo: {repo_name} | user: {user}",
+        )
+        ylabel = "MIP accuracy (doi/total_doi)"
+        xlabel = "Commit number (linear)"
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+
+        filename = path.join(result_folder, f"{user}.png")
+        plt.savefig(filename)
+        plt.close()
 
 
 def print_results(repo, visualize=False):
     mip = Mip(repo)
     csr = CsrFiles()
     result_folder = get_repo_result_dir(repo, [mip.alpha, mip.beta, mip.gamma])
-    data = deque()
-    users = defaultdict(int)
+    users_table = deque()
+    users_data = defaultdict(
+        lambda: {"commits": 0, "top3": 0, "top5": 0, "objects": 0, "score": 0.0})
     table_repo = pt(['commit', 'user', 'changed_objects', 'top 10 pred', 'score',
                      'top 3', 'top 5'])
-    table_users = pt(['user', 'score', 'num_objects', 'top 3', 'top 5'])
 
     total, total_3, total_5 = 0, 0, 0
     for j, commit in enumerate(X, 1):
@@ -53,9 +80,13 @@ def print_results(repo, visualize=False):
                                 "\n".join(str(a) for a in mip_ranking[:10]),
                                 prop_score, top_3, top_5])
 
-            users[username] += 1
-            data.append({"user": username,
-                         "commits": users[username],
+            users_data[username]["commits"] += 1
+            users_data[username]["top3"] += top_3
+            users_data[username]["top5"] += top_5
+            users_data[username]["objects"] += num_objects
+            users_data[username]["score"] += prop_score
+            users_table.append({"user": username,
+                         "commits": users_data[username]["commits"],
                          "accuracy": prop_score,
                          "top_3": top_3 / num_objects,
                          "top_5": top_5 / num_objects, })
@@ -64,13 +95,28 @@ def print_results(repo, visualize=False):
                 ranked = [a[0] for a in mip_ranking[:10]]
                 mip.drawMip(path.join(result_folder, str(j)), session.user,
                     objects, ranked, False)
+
+                draw_users(users_table, users_data, repo.name, result_folder)
+
         mip.updateMIP(session)
+
+    summarize_users = pt(['user', 'score', 'num_objects', 'top 3', 'top 5'])
+    for user, data in users_data.items():
+        summarize_users.add_row([user,
+                                 data["score"],
+                                 data["objects"],
+                                 data["top3"],
+                                 data["top5"],
+                                 ])
 
     with open(path.join(result_folder, 'res.txt'), 'w') as f:
         f.write(table_repo.get_string(title=str(mip)))
         f.write(f"\nTotal of {total} objects were emitted\n"
                 f"predicted {total_3} in the top-3\n"
                 f"predicted {total_5} in the top-5\n")
+        f.write("\n\nSummary:\n")
+        f.write(summarize_users.get_string(title="Users summary"))
+
 
 
 if __name__ == "__main__":
